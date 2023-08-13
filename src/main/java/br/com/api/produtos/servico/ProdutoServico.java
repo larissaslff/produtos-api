959e4ce854059dbe745f91aa1501c0f22cd01fb6
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-08-12 21:16:27 -0300

    estruturando o serviço

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,19 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
+
+    @Autowired
+    private ProdutoRepositorio repositorio;
+
+    public Iterable<ProdutoModelo> listar() {
+        return repositorio.findAll();
+    }
     
 }
