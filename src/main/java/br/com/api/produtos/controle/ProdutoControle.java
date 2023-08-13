1eca5733c501a76ee295b80b6573ab8859c858a8
larissaslff <110000521+larissaslff@users.noreply.github.com>
2023-08-12 22:18:35 -0300

    estruturando o post

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,14 +1,28 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController
 public class ProdutoControle {
 
-    @GetMapping("/")
-    public String rota() {
-        return "Funcionando";
+    @Autowired
+    private ProdutoServico servico;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return servico.listar();
     }
     
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo produtoModelo) {
+        return servico.cadastrar(produtoModelo);
+    }
 }
--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,9 +1,15 @@
 package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.deser.CreatorProperty;
+
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,8 +18,24 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio repositorio;
 
+    @Autowired
+    private RespostaModelo respostaModelo;
+
     public Iterable<ProdutoModelo> listar() {
         return repositorio.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(ProdutoModelo produtoModelo) {
+        if (produtoModelo.getNome().equals("")) {
+           respostaModelo.setMensagem("Nome obrigatório"); 
+           return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+        } else if (produtoModelo.getMarca().equals("")) {
+            respostaModelo.setMensagem("Marca obrigatória"); 
+           return new ResponseEntity<RespostaModelo>(respostaModelo, HttpStatus.BAD_REQUEST);
+        } else {
+            ProdutoModelo saved = repositorio.save(produtoModelo);
+            return new ResponseEntity<ProdutoModelo>(saved, HttpStatus.CREATED);
+        }
     }
     
 }
